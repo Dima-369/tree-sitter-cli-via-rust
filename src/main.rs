429a2bb5d031af24cd90b7590ad17d62dc4ccdc3
Dima Butemann <dbutemann@gmail.com>
2025-02-25 05:41:03 +0100

    Update all

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,89 @@
+use clap::{Arg, Command};
 use std::fs;
 use tree_sitter::Parser;
 use tree_sitter::Query;
 
-// TODO use click and allow setting language enum for kotlin and php
-// TODO use click and pass highlights string
+static LANGUAGES: [&str; 2] = ["kotlin", "php"];
+
+fn get_command() -> Command {
+    Command::new("Tree-sitter Syntax Highlighter")
+        .version("1.0")
+        .author("Dmytro Butemann <dbutemann@gmail.com>")
+        .about("Outputs capture names with byte range using Tree-sitter for Kotlin Emacs.")
+        .arg(
+            Arg::new("code")
+                .long("code")
+                .help("The code to parse")
+                .required(true),
+        )
+        .arg(
+            Arg::new("language")
+                .long("language")
+                .value_parser(LANGUAGES)
+                .required(true),
+        )
+        .arg(
+            Arg::new("highlights")
+                .long("highlights")
+                .help("String of highlights like the content of queries/highlights.scm")
+                .required(true),
+        )
+}
+
+fn create_parser(language: &str) -> Parser {
+    let mut parser = Parser::new();
+    let result = match language {
+        "kotlin" => parser.set_language(&tree_sitter_kotlin::language()),
+        "php" => {
+            let language = tree_sitter_php::LANGUAGE_PHP;
+            parser.set_language(&language.into())
+        }
+        _ => panic!("Unsupported language: {}", language),
+    };
+    // TODO show parsed error
+    result.expect(&format!(
+        "Error loading specified {} language grammar",
+        language
+    ))
+}
 
 fn main() {
+    /*    let matches = get_command().get_matches();
+        let code = matches.get_one::<String>("code").unwrap();
+        let language = matches.get_one::<String>("language").unwrap();
+        let highlights = matches.get_one::<String>("highlights").unwrap();
+    */
+    // TODO remove debug code
     let code = r#"
   data class Point(
     val x: Int,
     val y: Int
   )
 "#;
-    let mut parser = Parser::new();
-    parser
-        .set_language(&tree_sitter_kotlin::language())
-        .expect("Error loading Kotlin grammar");
-    let parsed = parser.parse(code, None);
-    let tree = parsed.unwrap();
-    let root_node = tree.root_node();
-
-    println!("{:#?}", root_node);
-    println!("{:#?}", root_node.kind());
-    println!("{:#?}", root_node.start_position());
-    println!("{:#?}", root_node.end_position());
-
-    let query_source = fs::read_to_string(
+    let language = "kotlin";
+    let highlights = fs::read_to_string(
         "/Users/dima/Developer/java-tree-sitter/tree-sitter-kotlin/queries/highlights.scm",
     )
-    .expect("Failed to read highlights.scm file");
-
-    let query =
-        Query::new(&tree_sitter_kotlin::language(), &*query_source).expect("Failed to create query");
-
+    .unwrap();
+    let parser = create_parser(language);
+    let parsed = parser.parse(code, None);
+    let tree = parsed.expect(&format!(
+        "Failed to parse passed code with language {}",
+        language
+    ));
+    let root_node = tree.root_node();
+    let query = Query::new(&tree_sitter_language, &*highlights).expect("Failed to create query");
     let mut query_cursor = tree_sitter::QueryCursor::new();
     let matches = query_cursor.matches(&query, root_node, code.as_bytes());
-
     for m in matches {
         for capture in m.captures {
             let node = capture.node;
             let capture_name = query.capture_names()[capture.index as usize];
             println!(
-                "Capture name: {}, text: {}",
+                "{} {} {}",
                 capture_name,
-                node.utf8_text(code.as_bytes()).unwrap()
+                node.byte_range().start,
+                node.byte_range().end
             );
         }
     }
