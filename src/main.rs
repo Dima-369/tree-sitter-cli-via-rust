e1e84f60c57692b7b1f0d98ae43d240138f465c6
Dima Butemann <dbutemann@gmail.com>
2025-02-25 10:10:35 +0100

    Update all

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use clap::{Arg, Command};
-use std::fs;
+use tree_sitter::Query;
 use tree_sitter::{Parser, StreamingIterator};
-use tree_sitter::{Query, Tree};
 
-static LANGUAGES: [&str; 2] = ["kotlin", "php"];
+static LANGUAGES: [&str; 7] = ["kotlin", "php", "bash", "dockerfile", "python", "java", "json"];
+enum Language {
+    Kotlin,
+    Php,
+    Bash,
+    Json,
+    Dockerfile,
+    Python,
+    Java,
+}
 
 fn get_command() -> Command {
     Command::new("Tree-sitter Syntax Highlighter")
@@ -30,55 +38,42 @@ fn get_command() -> Command {
         )
 }
 
-fn get_parser_language(language: &str) -> tree_sitter_language::LanguageFn {
+fn map_language_to_enum(language: &str) -> Language {
     match language {
-        "kotlin" => tree_sitter_kotlin_ng::LANGUAGE,
-        "php" => tree_sitter_php::LANGUAGE_PHP,
+        "kotlin" => Language::Kotlin,
+        "php" => Language::Php,
+        "bash" => Language::Bash,
+        "dockerfile" => Language::Dockerfile,
+        "python" => Language::Python,
+        "java" => Language::Java,
         _ => panic!("Unsupported language: {}", language),
     }
 }
 
-fn create_parser(language: &str, parser_language: tree_sitter_language::LanguageFn) -> Parser {
-    let mut parser = Parser::new();
-    match parser.set_language(&parser_language.into()) {
-        Ok(_) => parser,
-        Err(err) => panic!(
-            "Error loading specified {} language grammar: {:?}",
-            language, err
-        ),
-    }
-}
-
 fn main() {
-    /*    let matches = get_command().get_matches();
-        let code = matches.get_one::<String>("code").unwrap();
-        let language = matches.get_one::<String>("language").unwrap();
-        let highlights = matches.get_one::<String>("highlights").unwrap();
-    */
-    // TODO remove debug code
-    let code = r#"
-  data class Point(
-    val x: Int,
-    val y: Int
-  )
-"#;
-    let language = "php";
-    let highlights = fs::read_to_string(
-        "/Users/dima/Developer/java-tree-sitter/tree-sitter-kotlin/queries/highlights-backup.scm",
-    ).unwrap();
-    let parser_language = get_parser_language(language);
-    let mut parser = create_parser(language, parser_language);
-    let old_tree: Option<&Tree> = None;
-    let parsed = parser.parse(code, old_tree);
-    let tree = parsed.expect(&format!(
-        "Failed to parse passed code with language {}",
-        language
-    ));
+    let args = get_command().get_matches();
+    let code = args.get_one::<String>("code").unwrap();
+    let language = args.get_one::<String>("language").unwrap();
+    let highlights = args.get_one::<String>("highlights").unwrap();
+    let mut parser = Parser::new();
+    let language_enum = map_language_to_enum(language);
+    match language_enum {
+        Language::Kotlin => parser.set_language(&tree_sitter_kotlin::language()),
+        Language::Php => parser.set_language(&tree_sitter_php::LANGUAGE_PHP.into()),
+        Language::Bash => parser.set_language(&tree_sitter_bash::LANGUAGE.into()),
+        Language::Json => parser.set_language(&tree_sitter_json::LANGUAGE.into()),
+        // TODO fix this
+        Language::Dockerfile => parser.set_language(&tree_sitter_dockerfile::language().into()),
+        Language::Python => parser.set_language(&tree_sitter_python::LANGUAGE.into()),
+        Language::Java => parser.set_language(&tree_sitter_java::LANGUAGE.into()),
+    }
+    .expect(&format!("Error loading {} grammar", language));
+    let parsed = parser.parse(code, None);
+    let tree = parsed.unwrap();
     let root_node = tree.root_node();
-    // let query = Query::new(&tree_sitter_kotlin_ng::LANGUAGE.into(), &*highlights).expect("Failed to create query");
-    // TODO why does this crash?
-    let query = Query::new(&parser_language.into(), "(simple_identifier) @variable")
-        .expect("Failed to create query");
+    let parser_language = parser.language().unwrap();
+    let query = Query::new(&parser_language, highlights)
+        .expect("Failed to create query for passed highlights");
     let mut query_cursor = tree_sitter::QueryCursor::new();
     let mut matches = query_cursor.matches(&query, root_node, code.as_bytes());
     while let Some(m) = matches.next() {
