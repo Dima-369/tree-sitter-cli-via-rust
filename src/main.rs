d8fdc296a0d8060b8ca695a3ef0e94422c30782f
Dima Butemann <dbutemann@gmail.com>
2025-02-25 06:08:12 +0100

    Update all

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::{Arg, Command};
 use std::fs;
-use tree_sitter::Query;
 use tree_sitter::{Parser, StreamingIterator};
+use tree_sitter::{Query, Tree};
 
 static LANGUAGES: [&str; 2] = ["kotlin", "php"];
 
@@ -62,21 +62,23 @@ fn main() {
     val y: Int
   )
 "#;
-    let language = "kotlin";
+    let language = "php";
     let highlights = fs::read_to_string(
-        "/Users/dima/Developer/java-tree-sitter/tree-sitter-kotlin/queries/highlights.scm",
-    )
-    .unwrap();
+        "/Users/dima/Developer/java-tree-sitter/tree-sitter-kotlin/queries/highlights-backup.scm",
+    ).unwrap();
     let parser_language = get_parser_language(language);
     let mut parser = create_parser(language, parser_language);
-    let parsed = parser.parse(code, None);
+    let old_tree: Option<&Tree> = None;
+    let parsed = parser.parse(code, old_tree);
     let tree = parsed.expect(&format!(
         "Failed to parse passed code with language {}",
         language
     ));
     let root_node = tree.root_node();
     // let query = Query::new(&tree_sitter_kotlin_ng::LANGUAGE.into(), &*highlights).expect("Failed to create query");
-    let query = Query::new(&parser_language.into(), &*highlights).expect("Failed to create query");
+    // TODO why does this crash?
+    let query = Query::new(&parser_language.into(), "(simple_identifier) @variable")
+        .expect("Failed to create query");
     let mut query_cursor = tree_sitter::QueryCursor::new();
     let mut matches = query_cursor.matches(&query, root_node, code.as_bytes());
     while let Some(m) = matches.next() {
